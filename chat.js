7eeef631dd9706704319f6380a80ce789485251e
kokash1 <bdallhkwksh63@gmail.com>
2025-02-23 21:25:29 +0300

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,8 @@
+window.$crisp=[];
+window.CRISP_WEBSITE_ID="8fb59d64-0f9c-4f85-8e97-8483e7355aa6";
+(function(){d=document;
+    s=d.createElement("script");
+    s.src="https://client.crisp.chat/l.js";
+    s.async=1; 
+    d.getElementsByTagName("head")[0].appendChild(s);}
+)();
